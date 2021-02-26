bd68d6c38aba1e3a6029705c777ec107624609cb
Nishant Thakur <55345666+Nishantt23@users.noreply.github.com>
2021-02-26 18:33:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const container = document.getElementById('container');
+const text = document.getElementById('text');
+
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+Breathe();
+
+function Breathe() {
+  text.innerText = 'Breathe In!';
+  container.className = 'container grow';
+
+  setTimeout(() => {
+    text.innerText = 'Hold';
+
+    setTimeout(() => {
+      text.innerText = 'Breathe Out!';
+      container.className = 'container shrink';
+    }, holdTime);
+  }, breatheTime);
+}
+
+setInterval(Breathe, totalTime);
+
+function Volume() {
+    var audio = document.getElementById("sounds");
+    audio.volume = 0.2;
+}
+
+Volume();
